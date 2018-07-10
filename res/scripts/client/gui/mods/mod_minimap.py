b961e6e0d525b14d212d56cd93075e0d5a438d55
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2018-07-10 21:47:15 +0200

    Add HTTP server

--- res/scripts/client/gui/mods/mod_minimap.py
+++ res/scripts/client/gui/mods/mod_minimap.py
@@ -1,4 +1,7 @@
+import os
 import json
+import urllib
+import posixpath
 
 from helpers import dependency
 from helpers.dependency import _g_manager as dependency_manager
@@ -8,51 +11,121 @@ from gui.battle_control.arena_info.interfaces import IVehiclesAndPositionsContro
 from skeletons.gui.game_control import IGameController, IGameStateTracker
 
 from threading import Thread, RLock
+from SocketServer import TCPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 
 
-class ThreadedWebSocketServer:
-    def __init__(self, host='', port=8000, ws_factory=WebSocket):
-        self._host = host
-        self._port = port
-        self._ws_server = None
-        self._ws_factory = ws_factory
-        self._thread = None
-        self._close_lock = RLock()
-        self._closed = False
+class ConcurrentServer(object):
+    def __init__(self):
+        self._server = None
+        self.__thread = None
+        self.__while_running_lock = RLock()
+        self.__closed = False
 
     def start(self):
-        if self._thread is not None:
+        if self.__thread is not None:
             return
 
-        self._ws_server = SimpleWebSocketServer(self._host, self._port, self._ws_factory)
-        self._thread = Thread(target=self._serve)
-        self._thread.start()
+        self.__closed = False
+        self._server = self._create_server()
+        self.__thread = Thread(target=self.__serve)
+        self.__thread.start()
 
     def stop(self):
-        if self._ws_server is None:
+        if self._server is None:
             return
 
-        with self._close_lock:
-            self._ws_server.close()
-            self._closed = True
+        self.__closed = True
 
-        self._thread.join()
-        self._thread = None
+        with self.__while_running_lock:
+            self._close_server(self._server)
 
-    def _serve(self):
-        closed = False
-        while not closed:
-            with self._close_lock:
-                if not self._closed:
-                    self._ws_server.serveonce()
-                else:
-                    closed = True
+        self.__thread.join()
+        self.__thread = None
+        self._server = None
+
+    def _create_server(self):
+        raise NotImplementedError()
+
+    def _serve_once(self, server):
+        raise NotImplementedError()
+
+    def _close_server(self, server):
+        raise NotImplementedError()
+
+    def __serve(self):
+        with self.__while_running_lock:
+            while not self.__closed:
+                self._serve_once(self._server)
 
 
-class BroadcastingWebSocketServer(ThreadedWebSocketServer):
+class ConcurrentHTTPServer(ConcurrentServer):
+    def __init__(self, host='', port=8000, handler_factory=SimpleHTTPRequestHandler):
+        super(ConcurrentHTTPServer, self).__init__()
+        self._host = host
+        self._port = port
+        self._handler_factory = handler_factory
+
+    def _create_server(self):
+        return TCPServer((self._host, self._port), self._handler_factory)
+
+    def _serve_once(self, server):
+        server.handle_request()
+
+    def _close_server(self, server):
+        server.server_close()
+
+
+class ConcurrentWebSocketServer(ConcurrentServer):
+    def __init__(self, host='', port=8001, ws_factory=WebSocket):
+        super(ConcurrentWebSocketServer, self).__init__()
+        self._host = host
+        self._port = port
+        self._ws_factory = ws_factory
+
+    def _create_server(self):
+        return SimpleWebSocketServer(self._host, self._port, self._ws_factory)
+
+    def _serve_once(self, server):
+        server.serveonce()
+
+    def _close_server(self, server):
+        server.close()
+
+
+class DirectoryServingHTTPServer(ConcurrentHTTPServer):
+    def __init__(self, host='', port=8000, directory='.'):
+        handler_factory = DirectoryServingHTTPServer._make_handler_factory(directory)
+        super(DirectoryServingHTTPServer, self).__init__(host, port, handler_factory)
+
+    @staticmethod
+    def _make_handler_factory(directory):
+        directory = os.path.abspath(directory)
+
+        class HandlerFactory(SimpleHTTPRequestHandler):
+            def translate_path(self, path):
+                path = path.split('?', 1)[0]
+                path = path.split('#', 1)[0]
+                trailing_slash = path.rstrip().endswith('/')
+                path = posixpath.normpath(urllib.unquote(path))
+                words = path.split('/')
+                words = filter(None, words)
+                path = directory  # patch SimpleHTTPRequestHandler to use different directory than working dir
+                for word in words:
+                    if os.path.dirname(word) or word in (os.curdir, os.pardir):
+                        continue
+                    path = os.path.join(path, word)
+                if trailing_slash:
+                    path += '/'
+                return path
+
+        return HandlerFactory
+
+
+class BroadcastingWebSocketServer(ConcurrentWebSocketServer):
     def broadcast(self, msg):
-        for client in self._ws_server.connections.itervalues():
+        for client in self._server.connections.itervalues():
             client.sendMessage(msg)
 
 
@@ -66,12 +139,15 @@ class ExternalMinimapController(IExternalMinimapController, IVehiclesAndPosition
     def __init__(self):
         super(ExternalMinimapController, self).__init__()
         self._isBattleCtrlInit = False
-        self._broadcaster = BroadcastingWebSocketServer()
+        self._http_server = DirectoryServingHTTPServer(port=13370, directory='mods/files/minimap_frontend')
+        self._broadcaster = BroadcastingWebSocketServer(port=13371)
 
     def start(self):
+        self._http_server.start()
         self._broadcaster.start()
 
     def stop(self):
+        self._http_server.stop()
         self._broadcaster.stop()
 
     def onAvatarBecomePlayer(self):
